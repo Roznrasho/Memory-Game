5c0206c33a7b212bf2f7f01f84d48b8e3600d3e7
Monika-Choudhary <monikachahota@gmail.com>
2025-01-21 12:30:06 +0100

    feat: update game state variables; created file checkMatched; started adding checkMatch functionality and added import/export

---
+++ src/featurs/checkMatch.ts
@@ -0,0 +1,23 @@
+import { flippedCards, cards, matchedPairs, } from '../main';
+import { renderCards } from './shuffleAndRender';
+import { endGame } from ';
+
+export { checkMatch, endGame };
+
+function checkMatch(): void {
+    const [card1, card2] = matchedPairs;
+    if (card1.value === card2.value) {
+      card1.isMatched = true;
+      card2.isMatched = true;
+      
+      if (cards.every(card => card.isMatched)) {
+        endGame();
+      }
+    } else {
+      card1.isFlipped = false;
+      card2.isFlipped = false;
+    }
+    
+   matchedPairs;
+    renderCards();
+  }
--- src/main.ts
+++ src/main.ts
@@ -14,20 +14,23 @@ const movesElement = document.getElementById("moves");
 const restartButton = document.getElementById("restartButton");
 
 //game state variables
-let cards: HTMLElement[] = []; // array to store all cards / Array zum Speichern aller Karten
-let flippedCards: HTMLElement[] = []; // array to store flipped cards / Array zum Speichern der umgedrehten Karten
+let cards: Card[] = []; // array to store all cards / Array zum Speichern aller Karten
+let flippedCards: Card[] = []; // array to store flipped cards / Array zum Speichern der umgedrehten Karten
 let matchedPairs = 0; // number of matched pairs / Anzahl der übereinstimmenden Paare
-let moves = 0; // number of moves / Anzahl der Züge
-let timer = 0; // timer in seconds / Timer in Sekunden
-let timerInterval: number | undefined; // interval to update timer / Intervall zum Aktualisieren des Timers
-let gameStarted = false; // flag to check if game has started / Flagge zur Überprüfung, ob das Spiel gestartet wurde
+let moves: number = 0; // number of moves / Anzahl der Züge
+let timer: number = 0; // timer in seconds / Timer in Sekunden
+let timerInterval: number | null; // interval to update timer / Intervall zum Aktualisieren des Timers
+let gameStarted: boolean = false; // flag to check if game has started / Flagge zur Überprüfung, ob das Spiel gestartet wurde
+let movesDisplay: HTMLElement; // reference to moves display / Referenz auf Anzeige der Züge
+let timeDisplay: HTMLElement; 
+let gameBoard: HTMLElement;
 
-
-
+export { initialzeGame, cards, flippedCards, matchedPairs, moves, timer, timerInterval, gameStarted, movesDisplay, restartButton, timeDisplay, gameBoard };
 
 function initialzeGame(): void {
     const emojis = ["🎲", "🎨", "🎭","⚽", "♟️", "🎁", "🏆", "🚀", "🎪","⚓", "🪭", "⚜️" ];
-    const cards = [...emojis, ...emojis].map((value, index) => ({
+    
+     cards = [...emojis, ...emojis].map((value, index) => ({
         id: index,
         value,
         isFlipped: false,
