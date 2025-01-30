b701fe7b5bc72d9b632ebee166283558fd5a6c3a
hana <hanaabrham89@gmail.com>
2025-01-30 09:27:14 +0100

    refactor: clean up HTML structure and improve button setup logic

--- src/features/setUpGameButton.ts
+++ src/features/setUpGameButton.ts
@@ -1,23 +1,30 @@
 import { initializeGame } from "./initializeGame";
 
 function setUpGameButtons() {
-    const start12Button = document.getElementById("start-12") as HTMLButtonElement;
-    const start18Button = document.getElementById("start-18") as HTMLButtonElement;
-    const start24Button = document.getElementById("start-24") as HTMLButtonElement;
+  const start12Button = document.getElementById(
+    "start-12"
+  ) as HTMLButtonElement;
+  const start18Button = document.getElementById(
+    "start-18"
+  ) as HTMLButtonElement;
+  
+  console.log('start:',start18Button);
+  const start24Button = document.getElementById(
+    "start-24"
+  ) as HTMLButtonElement;
+  console.log('start24Button:',start24Button);
 
-    start12Button?.addEventListener("click", () => {
-        initializeGame(12);
-    });
-
- 
-
-    start24Button?.addEventListener("click", () => {
-        initializeGame(24);
-    });
+  start12Button?.addEventListener("click", () => {
+    initializeGame(12);
+  });
+  start18Button?.addEventListener("click", () => {
+    initializeGame(18);
    
-start18Button?.addEventListener("click", () => {
-    initializeGame(18); 
-});
+  });
 
+  start24Button?.addEventListener("click", () => {
+    initializeGame(24);
+  });
 }
+console.log('setUpGameButtons:',setUpGameButtons);
 export { setUpGameButtons };
